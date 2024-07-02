b70d20394a0993083485c9b1f142164fba118e18
woic-ej <woic0102@naver.com>
2024-07-02 20:56:45 +0900

    Feature: 개발 완료

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "날씨 앱",
+  description: "오늘의 날씨를 알려드립니다",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import style from "./style.module.css";
+import { getCurrentWeather } from "@/utils/getCurrentWeather";
+import getTime from "../utils/getTime";
+import RevalidateButton from "@/components/RevalidateButton";
+
+export default async function Home() {
+  const res = await getCurrentWeather("seoul");
+  const time = await getTime(res.location.tz_id);
+
+  return (
+    <>
+      <h1>날씨 앱</h1>
+      <h3>{time.dateTime}</h3>
+      <ul className={style.list}>
+        <li>
+          <Link href="/seoul?name=서울">서울</Link>
+          <span>{res.current.condition.text}</span>
+        </li>
+        <li>
+          <Link href="/NYC?name=뉴욕">뉴욕</Link>
+        </li>
+        <li>
+          <Link href="/london?name=런던">런던</Link>
+        </li>
+      </ul>
+      <RevalidateButton tag={"time"} />
+    </>
+  );
+}
